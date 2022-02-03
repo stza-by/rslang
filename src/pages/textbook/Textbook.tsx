0a15a8efd524b5c126e15951f822023fc62cebb2
YS <yulia.deamchik@gmail.com>
2022-02-03 16:09:06 +0300

    feat: add page textbook

---
+++ src/pages/textbook/Textbook.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './textbook.module.css';
+
+const Textbook: React.FC = () => (
+  <main className={style.container}>
+    <p className='text-4xl font-semibold text-gray-700 mb-7'>Учебник</p>
+    <div className={style.ul__wrapper}>
+    <ul className={`${style.ul} text-white`}>
+      <li className='bg-blue-200'><Link className={style.link} to='/'><span>A1</span>Beginner</Link></li>
+      <li className='bg-blue-400'><Link className={style.link} to='/'><span>A2</span>Elementary</Link></li>
+      <li className='bg-violet-400'><Link className={style.link} to='/'><span>B1</span>Intermediate</Link></li>
+      <li className='bg-violet-600'><Link className={style.link} to='/'><span>B2</span>Upper<br />Intermediate</Link></li>
+    </ul>
+    <ul className={`${style.ul} text-white`}>
+      <li className='bg-rose-300'><Link className={style.link} to='/'><span>C1</span>Advanced</Link></li>
+      <li className='bg-rose-400'><Link className={style.link} to='/'><span>C2</span>Proficiency</Link></li>
+    </ul>
+    <ul className={`${style.ul} ${style.ul__difficult} text-white`}>
+      <li className='bg-amber-400'><Link className={style.link} to='/'>Сложные слова</Link></li>
+    </ul>
+    </div>
+
+  </main>
+);
+
+export default Textbook;
