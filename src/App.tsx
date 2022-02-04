db8992a3326b2c405bb3f107cf475c3f214a8df7
GenaVinokurov <genabrest97@gmail.com>
2022-02-04 15:29:54 +0300

    add: card template

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Main from './pages/main/Main';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Card from './components/card/Card';
 
 const App: React.FC = () => (
   <>
     <Header></Header>
     <div className="container">
       <Main></Main>
+      <Card></Card>
     </div>
     <Footer></Footer>
   </>
