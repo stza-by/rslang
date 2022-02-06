2d94989972107491287fb814391d2d894d4a26d0
GenaVinokurov <50237079+GenaVinokurov@users.noreply.github.com>
2022-02-06 11:23:21 +0300

    Card (#9)
    * add: card template
    
    * refactor: remove card from App
    
    Co-authored-by: GenaVinokurov <genabrest97@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Main from './pages/main/Main';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Card from './components/card/Card';
 
 const App: React.FC = () => (
   <>
