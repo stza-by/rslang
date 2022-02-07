1e5c2dd618d02d40b6ca477c4ff2a19c98fd38ff
pnv13 <ivanpakhotnykh@mail.ru>
2022-02-07 17:59:49 +0300

    feat: complete mini-games page (#15)
    * feat: complete mini-games page
    
    * fix: fix routing to the game

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => (
   <nav>
     <ul className={style.ul}>
       <li><Link to='/textbook' className={style.link}>Учебник</Link></li>
-      <li><Link to='/' className={style.link}>Мини-игры</Link></li>
+      <li><Link to='/games' className={style.link}>Мини-игры</Link></li>
       <li><Link to='/' className={style.link}>Статистика</Link></li>
     </ul>
   </nav>
---
+++ src/pages/games/AudioGame.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AudioGame: React.FC = () => (
+  <main>
+    <div>Аудиовызов</div>
+  </main>
+);
+
+export default AudioGame;
