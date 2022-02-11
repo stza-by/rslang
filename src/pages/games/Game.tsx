676ed6e78620328a16122ccb50dd3bb5b778565f
pnv13 <ivanpakhotnykh@mail.ru>
2022-02-11 20:23:13 +0300

    feat: add difficult levels in games

--- src/pages/games/Game.tsx
+++ src/pages/games/Game.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { IGameProps } from '../../services/types';
+import { IGameDataProps } from '../../services/types';
 
-const Game: React.FC<IGameProps> = ({ game }) => {
+const Game: React.FC<IGameDataProps> = ({ game }) => {
   const { name, img, description, rout } = game;
 
   return (
--- src/pages/games/SprintGame.tsx
+++ src/pages/games/SprintGame.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { IGameProps } from '../../services/types';
 
-const SprintGame: React.FC = () => (
-    <div>Спринт</div>
+const SprintGame: React.FC<IGameProps> = ({ difficultLvl }) => (
+  <div>
+    Спринт
+    <div>Сложность {difficultLvl}</div>
+  </div>
 );
 
 export default SprintGame;
