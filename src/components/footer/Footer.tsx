747038479a9e430635aaca37c18472b9ba654552
Ivan Pakhotnykh <ivanpakhotnykh@mail.ru>
2022-02-21 22:19:12 +0300

    Audio keyboard (#28)
    * refactor: delete unnecessary function from card and delete consolelog from app
    
    * feat: add keyboard control
    
    * fix: footer
    
    * fix: aboutUs

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,14 +4,14 @@ import style from './Footer.module.css';
 const Footer = () => (
   <footer className={`${style.container} bg-main-orange`}>
     <div className={style.wrapper}>
-      <a href='https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-2/rs-lang/rslang.md' className={style.task}>2022 RSLang</a>
+      <a href='https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-2/rs-lang/rslang.md' className={style.task} target='_blank' rel="noreferrer">2022 RSLang</a>
       <ul className={style.developers}>
-        <li><a href='https://github.com/YuliaSavchik'>YuliaSavchik</a></li>
-        <li><a href='https://github.com/GenaVinokurov'>GenaVinokurov</a></li>
-        <li><a href='https://github.com/pnv13'>pnv13</a></li>
+        <li><a href='https://github.com/Helen-JS' target='_blank' rel="noreferrer">Helen-JS</a></li>
+        <li><a href='https://github.com/GenaVinokurov' target='_blank' rel="noreferrer">GenaVinokurov</a></li>
+        <li><a href='https://github.com/pnv13' target='_blank' rel="noreferrer">pnv13</a></li>
       </ul>
-      <a href='https://rs.school/' className={style.school__logo}>
-        <div className={style.img}></div>
+      <a href='https://rs.school/' className={style.school__logo} target='_blank' rel="noreferrer">
+        <div className={style.img} />
       </a>
     </div>
   </footer>
